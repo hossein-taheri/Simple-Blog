f53c5c58b9115107eea486ed86215260efb6ca56
Hossein <you@example.com>
2021-10-04 13:49:56 +0330

    Some bugs have been fixed

--- middleware/JWTAuth.js
+++ middleware/JWTAuth.js
@@ -1,42 +1,33 @@
 const JWT = require('../helpers/JWT');
 const ApiResponse = require('../helpers/responses/ApiResponse');
+const {Unauthorized} = require("../helpers/CustomErrors");
 
 const JWTAuth = {
     check: async (req, res, next) => {
-        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-            let token = req.headers.authorization.split(' ')[1];
-            if (!token) {
-                return ApiResponse
-                    .error(
-                        req,
-                        res,
-                        401,
-                        "Incorrect token"
-                    );
-            } else {
-                try {
-                    let decoded = await JWT
-                        .verifyAccessToken(token)
-                    req.user_id = decoded.id;
-                    next();
-                } catch (err) {
-                    return ApiResponse
-                        .error(
-                            req,
-                            res,
-                            401,
-                            "Incorrect token"
-                        );
-                }
+        try {
+            if (!(req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer')) {
+                throw new Unauthorized("Incorrect token")
             }
-        } else {
+
+            let token = req.headers.authorization.split(' ')[1];
+
+            if (!token) {
+                throw new Unauthorized("Incorrect token")
+            }
+
+            let decoded = await JWT.verifyAccessToken(token)
+
+            req.user_id = decoded.id;
+
+            next();
+        } catch (err) {
             return ApiResponse
                 .error(
                     req,
                     res,
-                    401,
-                    "Incorrect token"
-                );
+                    err.code || 401,
+                    err.message
+                )
         }
     },
 }
